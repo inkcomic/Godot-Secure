8e9728fbd77b7886c127e66007b1a46ed674918c
inkcomic <inkcomic@gmail.com>
2026-01-28 21:25:15 +0800

    update

---
+++ rebuild_win.bat
@@ -0,0 +1,28 @@
+@echo off
+set GODOT_DIR=godot
+set BRANCH_NAME=4.6
+set MY_LOCAL_SOURCE=C:/Users/Admin/MyLocalNugetSource/
+$env:SCRIPT_AES256_ENCRYPTION_KEY = Get-Content godot.gdkey
+
+git clone https://github.com/godotengine/godot.git
+cd godot
+git fetch origin %BRANCH_NAME%
+git checkout -b %BRANCH_NAME% origin/%BRANCH_NAME%
+git pull origin %BRANCH_NAME%
+
+::  openssl rand -hex 32 > godot.gdkey 
+
+py ./modules/mono/build_scripts/build_assemblies.py --godot-output-dir ./bin --push-nupkgs-local %MY_LOCAL_SOURCE%
+dotnet nuget add source %MY_LOCAL_SOURCE% --name MyLocalNugetSource
+bin/godot.windows.editor.x86_64.mono --headless --generate-mono-glue modules/mono/glue
+
+scons platform=windows target=editor module_mono_enabled=yes
+
+scons platform=windows target=template_debug module_mono_enabled=yes
+scons platform=windows target=template_release module_mono_enabled=yes
+
+scons platform=android target=template_release arch=arm32 module_mono_enabled=yes
+scons platform=android target=template_release arch=arm64 generate_android_binaries=yes module_mono_enabled=yes
+
+scons platform=android target=template_debug arch=arm32 module_mono_enabled=yes
+scons platform=android target=template_debug arch=arm64 generate_android_binaries=yes module_mono_enabled=yes
