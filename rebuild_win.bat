82f6fb186e2be004c98d8e7721a2820bd0ff7c95
inkcomic <inkcomic@gmail.com>
2026-02-24 02:09:44 +0800

    update

--- rebuild_win.bat
+++ rebuild_win.bat
@@ -5,14 +5,13 @@ set MY_LOCAL_SOURCE=C:/Users/Admin/MyLocalNugetSource
 @echo on
 $env:SCRIPT_AES256_ENCRYPTION_KEY = Get-Content godot.gdkey
 
-
 git clone https://github.com/godotengine/godot.git
 cd godot
 git fetch origin %BRANCH_NAME%
 git checkout -b %BRANCH_NAME% origin/%BRANCH_NAME%
 git pull origin %BRANCH_NAME%
 
-openssl rand -hex 32 > godot/godot.gdkey 
+openssl rand -hex 32 > godot.gdkey 
 
 py ../Godot.Secure.AES-256.py ./
 
@@ -27,6 +26,7 @@ scons platform=android target=template_release arch=arm64 generate_android_binar
 scons platform=android target=template_debug arch=arm32 module_mono_enabled=yes
 scons platform=android target=template_debug arch=arm64 generate_android_binaries=yes module_mono_enabled=yes
 
-bin/godot.windows.editor.x86_64.mono.exe --headless --generate-mono-glue modules/mono/glue
+./bin/godot.windows.editor.x86_64.mono.exe --headless --generate-mono-glue modules/mono/glue
 py ./modules/mono/build_scripts/build_assemblies.py --godot-output-dir ./bin --push-nupkgs-local %MY_LOCAL_SOURCE%
+dotnet nuget remove source MyLocalNugetSource
 dotnet nuget add source %MY_LOCAL_SOURCE% --name MyLocalNugetSource
